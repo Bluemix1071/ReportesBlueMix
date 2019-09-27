839466d2577d3c20e959ff89c1a59cbe62bba488
felipe1430 <felipe.martinez.hernandezh1@gmail.com>
2019-09-27 15:13:46 -0400

    xd

--- app/Http/Controllers/Admin/EditarUserController.php
+++ app/Http/Controllers/Admin/EditarUserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use DB;
+use App\User;
 
 class EditarUserController extends Controller
 {
@@ -18,6 +19,24 @@ class EditarUserController extends Controller
         $editar=DB::table('users')->select('id','name','email','tipo_usuario','estado')->get();
         return view('admin.ListarUser',compact('editar'));
     }
+
+
+    public function update(Request $request)
+    {
+
+        $usuario=User::findOrfail($request->id);
+        $usuario->id=$request->get('id');
+        $usuario->name=$request->get('name');
+        $usuario->email=$request->get('email');
+        $usuario->tipo_usuario=$request->get('tipo');
+        $usuario->estado=$request->get('Estado');
+        $usuario->update();
+        return back();
+        //xd
+    }
+
+    
+   
 
 
 
