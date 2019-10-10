9786144b1f3f5416f32086248fba4f44f8cfca8a
Rokax1 <leandrofernando739@gmail.com>
2019-10-10 12:23:34 -0300

    arreglo excel 2

--- app/Exports/AdminExport.php
+++ app/Exports/AdminExport.php
@@ -8,17 +8,32 @@ use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\FromQuery;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use DB;
+use Illuminate\Contracts\View\View;
 
-class AdminExport implements FromCollection
+
+class AdminExport implements FromView
 {
     /**
     * @return \Illuminate\Support\Collection
     */
+
+    public function view(): View
+    {
+        return view('exports.productos_negativos', [
+            'productos_negativos' => DB::table('Productos_negativos')->get()
+        ]);
+    }
+
+
+
+
+
+    /*
     public function collection()
     {
         return $productos=DB::table('Productos_negativos')->get();
     }
-    
+    */
     
 }
 
