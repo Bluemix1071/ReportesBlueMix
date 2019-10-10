c3645bcd366acd89aa4c779a77aa625eac1c8930
Rokax1 <leandrofernando739@gmail.com>
2019-10-10 09:59:17 -0300

    arreglo excel

--- resources/views/exports/productos_negativos.blade.php
+++ resources/views/exports/productos_negativos.blade.php
@@ -1,15 +1,21 @@
 <table>
     <thead>
     <tr>
-        <th>Name</th>
-        <th>Email</th>
-        <th>ajdjasjdadj</th>
+        <th>Nombre</th>
+        <th>Ubicacion</th>
+        <th>Codigo</th>
+        <th>Bodega stock</th>
+        <th>Sala stock</th>
     </tr>
     </thead>
     <tbody>
     @foreach($productos_negativos as $xd)
         <tr>
             <td>{{ $xd->nombre }}</td>
+            <td>{{ $xd->ubicacion }}</td>
+            <td>{{ $xd->codigo }}</td>
+            <td>{{ $xd->bodega_stock }}</td>
+            <td>{{ $xd->sala_stock }}</td>
          
         </tr>
     @endforeach
