1142d2d8bab771410eaf5259793b30135f52950d
Rokax1 <leandrofernando739@gmail.com>
2019-10-29 17:48:21 -0300

    xd

--- routes/web.php
+++ routes/web.php
@@ -61,7 +61,7 @@ Route::prefix('admin')->namespace('Admin')->middleware('auth','SuperAdmin')->gro
     //---------------------Exportaciones----------------------//
   
     Route::get('/pdf/{NroOrden}','exports\ExportsController@exportpdf')->name('pdf.orden');
-    Route::get('/excelproductospormarca','exports\ExportsController@exportExcelproductospormarca')->name('excelproductopormarca');
+    Route::post('/excelproductospormarca','exports\ExportsController@exportExcelproductospormarca')->name('excelproductopormarca');
     Route::post('/ExcelDesv','exports\ExportsController@exportExcelDesviacion')->name('excelDesviacion');
 
 
