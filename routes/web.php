bf663702ba326ab3a336100d469fced091ad85ea
Rokax1 <leandrofernando739@gmail.com>
2019-11-07 17:22:39 -0300

    xd

--- routes/web.php
+++ routes/web.php
@@ -9,21 +9,22 @@ Route::get('/logout','seguridad\LoginController@logout')->name('logout');
 Route::get('/graficos', 'ChartControllers\PulseChartController@index')->name('chart');
 Route::post('/graficos', 'ChartControllers\PulseChartController@cargarC3')->name('cargarChart');
 
-Route::get('/sala/Cambiodeprecios','sala\SalaController@index')->name('cambiodeprecios');
-Route::post('/sala/Cambiodeprecios','sala\SalaController@filtrarcambioprecios')->name('filtrarcambioprecios');
 
 
 
-
-//Route::get('/home', 'HomeController@index')->name('home');
+//Rutas autenticadas de sala
+Route::prefix('Ssla')->namespace('sala')->middleware('auth')->group(function(){
+    Route::get('/Cambiodeprecios','SalaController@index')->name('cambiodeprecios');
+    Route::post('/Cambiodeprecios','SalaController@filtrarcambioprecios')->name('filtrarcambioprecios');
+});
 
 
 //rutas globales autenticadas
 Route::prefix('publicos')->middleware('auth')->group(function(){
 
     Route::get('/','InicioController@index')->name('Publico');
-    Route::get('/ProductosNegativos','publico\PublicoController@index')->name('ProductosNegativos');
     Route::post('/ProductosNegativos','publico\PublicoController@filtarProductosNegativos')->name('filtrar');
+    Route::get('/ProductosNegativos','publico\PublicoController@index')->name('ProductosNegativos');
     Route::get('/ProductosNegativos2','publico\PublicoController@listarFiltrados')->name('filtro2');
     Route::get('/Informacion','publico\PublicoController@informacion')->name('informacion');
   
