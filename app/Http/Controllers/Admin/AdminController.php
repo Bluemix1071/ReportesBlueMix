8e18d77328d46c71880528e8ecac6089ef922997
Rokax1 <leandrofernando739@gmail.com>
2019-11-14 17:53:15 -0300

    xdd

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -239,23 +239,18 @@ class AdminController extends Controller
        tiempo Between 90000 And 95959 group by tipo', [$fecha1,$fecha2]);
        //convertir array en coleccion
       
+    
       if(empty($doc1)){
 
-        dd('xddd');
-      }
 
+      }else{
         $TotalBoleta = $doc1[0]->bruto;
         $TotalFactura =$doc1[1]->bruto;
         $TotalCantBoletas =$doc1[0]->cantidad;
         $TotalCantFacturas =$doc1[1]->cantidad;
         $collection = Collection::make($doc1);
-     
+      }
 
-    
-
-       
-
-     
 
       //Rango de las 10:00:00 a las 10:59:59
        $doc2=DB::select( 'select
@@ -266,15 +261,18 @@ class AdminController extends Controller
        where
        fecha_real between ? and ? and
        tiempo Between 100000 And 105959 group by tipo', [$fecha1,$fecha2]);
+       if(empty($doc2)){
+
+       }else{
       
        $TotalBoleta = $TotalBoleta + $doc2[0]->bruto;
        $TotalFactura =$TotalFactura + $doc2[1]->bruto;
        $TotalCantBoletas = $TotalCantBoletas+$doc2[0]->cantidad;
        $TotalCantFacturas =$TotalCantFacturas+$doc2[1]->cantidad;
-
+       
         //añadir mas rangos a la coleccion 
        $collection = $collection->merge(Collection::make($doc2)); 
-      
+       }
 
        //Rango de las 11:00:00 a las 11:59:59
        $doc3=DB::select('select
@@ -285,7 +283,10 @@ class AdminController extends Controller
        where
        fecha_real between ? and ? and
        tiempo Between 110000 And 115959 group by tipo', [$fecha1,$fecha2]);
+       if(empty($doc3)){
 
+
+       }else{
        
        $TotalBoleta = $TotalBoleta + $doc3[0]->bruto;
        $TotalFactura =$TotalFactura + $doc3[1]->bruto;
@@ -294,7 +295,7 @@ class AdminController extends Controller
        $TotalCantFacturas =$TotalCantFacturas+$doc3[1]->cantidad;
 
        $collection = $collection->merge(Collection::make($doc3)); 
-
+       }
       
         //Rango de las 12:00:00 a las 12:59:59
        $doc4=DB::select('select
@@ -305,6 +306,11 @@ class AdminController extends Controller
        where
        fecha_real between ? and ? and
        tiempo Between 120000 And 125959 group by tipo', [$fecha1,$fecha2]);
+
+       if(empty($doc4)){
+
+
+       }else{
      
        $TotalBoleta = $TotalBoleta + $doc4[0]->bruto;
        $TotalFactura =$TotalFactura + $doc4[1]->bruto;
@@ -313,7 +319,7 @@ class AdminController extends Controller
        $TotalCantFacturas =$TotalCantFacturas+$doc4[1]->cantidad;
 
        $collection = $collection->merge(Collection::make($doc4)); 
-      
+       }  
       //ango de las 13:00:00 a las 13:59:59
        $doc5=DB::select('select
        tipo as "Tipo" , 
@@ -323,6 +329,10 @@ class AdminController extends Controller
        where
        fecha_real between ? and ? and
        tiempo Between 130000 And 135959 group by tipo' , [$fecha1,$fecha2]);
+
+       if(empty($doc5)){
+
+       }else{
       
        $TotalBoleta = $TotalBoleta + $doc5[0]->bruto;
        $TotalFactura =$TotalFactura + $doc5[1]->bruto;
@@ -331,7 +341,7 @@ class AdminController extends Controller
        $TotalCantFacturas =$TotalCantFacturas+$doc5[1]->cantidad;
 
        $collection = $collection->merge(Collection::make($doc5)); 
-      
+       }
         //Rango de las 14:00:00 a las 14:59:59
         $doc6=DB::select('select
         tipo as "Tipo" , 
@@ -341,6 +351,9 @@ class AdminController extends Controller
         where
         fecha_real between ? and ? and
         tiempo Between 140000 And 145959 group by tipo', [$fecha1,$fecha2]);
+        if(empty($doc6)){
+
+        }else{
        
         
         $TotalBoleta = $TotalBoleta + $doc6[0]->bruto;
@@ -350,7 +363,7 @@ class AdminController extends Controller
         $TotalCantFacturas =$TotalCantFacturas+$doc6[1]->cantidad;
         
         $collection = $collection->merge(Collection::make($doc6)); 
-        
+        }
         //Rango de las 15:00:00 a las 15:59:59
         $doc7=DB::select('select
         tipo as "Tipo", 
@@ -361,6 +374,10 @@ class AdminController extends Controller
         fecha_real between ? and ? and
         tiempo Between 150000 And 155959 group by tipo', [$fecha1,$fecha2]);
         
+        //dd($doc7);
+        if(empty($doc7)){
+         
+        }else{
         $TotalBoleta = $TotalBoleta + $doc7[0]->bruto;
         $TotalFactura =$TotalFactura + $doc7[1]->bruto;
 
@@ -369,6 +386,7 @@ class AdminController extends Controller
         
         $collection = $collection->merge(Collection::make($doc7));
         
+        }
         //Rango de las 16:00:00 a las 16:59:59
         $doc8=DB::select('select
         tipo as "Tipo" , 
@@ -379,6 +397,10 @@ class AdminController extends Controller
         fecha_real between ? and ? and
         tiempo Between 160000 And 165959 group by tipo', [$fecha1,$fecha2]);
        
+        if(empty($doc8)){
+
+
+        }else{
         $TotalBoleta = $TotalBoleta + $doc8[0]->bruto;
         $TotalFactura =$TotalFactura + $doc8[1]->bruto;
 
@@ -386,7 +408,7 @@ class AdminController extends Controller
         $TotalCantFacturas =$TotalCantFacturas+$doc8[1]->cantidad;
         
         $collection = $collection->merge(Collection::make($doc8));
-        
+       }
         //Rango de las 17:00:00 a las 17:59:59
         $doc9=DB::select('select
         tipo as "Tipo" , 
@@ -397,6 +419,10 @@ class AdminController extends Controller
         fecha_real between ? and ? and
         tiempo Between 170000 And 175959 group by tipo', [$fecha1,$fecha2]);
        
+        if(empty($doc9)){
+
+
+        }else{
         $TotalBoleta = $TotalBoleta + $doc9[0]->bruto;
         $TotalFactura =$TotalFactura + $doc9[1]->bruto;
 
@@ -404,7 +430,7 @@ class AdminController extends Controller
         $TotalCantFacturas =$TotalCantFacturas+$doc9[1]->cantidad;
 
         $collection = $collection->merge(Collection::make($doc9));  
-        
+        }
         //Rango de las 18:00:00 a las 18:59:59
         $doc10=DB::select('select
         tipo as "Tipo" , 
@@ -414,6 +440,10 @@ class AdminController extends Controller
         where
         fecha_real between ? and ? and
         tiempo Between 180000 And 185959 group by tipo', [$fecha1,$fecha2]);
+
+        if(empty($doc10)){
+
+        }else{
        
         $TotalBoleta = $TotalBoleta + $doc10[0]->bruto;
         $TotalFactura =$TotalFactura + $doc10[1]->bruto;
@@ -422,7 +452,7 @@ class AdminController extends Controller
         $TotalCantFacturas =$TotalCantFacturas+$doc10[1]->cantidad;
         
         $collection = $collection->merge(Collection::make($doc10));
-        
+        }
         //Rango de las 19:00:00 a las 19:59:59
         $doc11=DB::select('select
         tipo as "Tipo" , 
@@ -432,7 +462,11 @@ class AdminController extends Controller
         where
         fecha_real between ? and ? and
         tiempo Between 190000 And 195959 group by tipo', [$fecha1,$fecha2]);
-        
+        if(empty($doc11)){
+
+
+        }else{
+      
         $TotalBoleta = $TotalBoleta + $doc11[0]->bruto;
         $TotalFactura =$TotalFactura + $doc11[1]->bruto;
         
@@ -440,7 +474,7 @@ class AdminController extends Controller
         $TotalCantFacturas =$TotalCantFacturas+$doc11[1]->cantidad;
 
         $collection = $collection->merge(Collection::make($doc11)); 
-        
+        }
          //Rango de las 20:00:00 a las 20:59:59
          $doc12=DB::select('select
          tipo as "Tipo" , 
@@ -452,7 +486,16 @@ class AdminController extends Controller
          tiempo Between 200000 And 205959 group by tipo', [$fecha1,$fecha2]);
          
          
-         $collection = $collection->merge(Collection::make($doc12)); 
+         if(empty($collection)){
+         
+          $collection=null;
+          $TotalBoleta = 0;
+          $TotalFactura =0;
+          $TotalCantBoletas = 0;
+          $TotalCantFacturas =0;
+
+         }
+       
     
      
 
