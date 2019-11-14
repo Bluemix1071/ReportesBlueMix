f61e83b173b402166f2db6452b21d45069455346
felipe1430 <felipe.martinez.hernandezh1@gmail.com>
2019-11-14 11:25:57 -0300

    compras fecha

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -3,13 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use DB;
 
 class InicioController extends Controller
 {
     public function index()
     {
-   
+        $date = Carbon::now();
+        $date = $date->format('d-m-Y');
+        $compras=DB::table('comprasdehoy')->get();
+        $variable1=$compras[0]->id;
+        // dd($variable1,$compras);
   
-    return view('publicos.index');
+    return view('publicos.index',compact('date','variable1'));
     }
+
+
+
+
 }
