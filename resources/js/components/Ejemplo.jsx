b91e2d7892ad9244ca82a4206dfae2cdf980029d
Rokax1 <leandrofernando739@gmail.com>
2020-11-09 22:17:08 -0300

    React instalado

---
+++ resources/js/components/Ejemplo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+
+const Ejemplo = () => {
+    return (
+       <h1>Componente de react en laravel</h1>
+     );
+}
+
+export default Ejemplo;
