93c441bf95e4a80a350ff8c0726b17dd5e1ee773
Ignacio <ignaciobarrera4@gmail.com>
2022-11-10 12:24:44 -0300

    Prueba Agregar Curso v6

--- app/Http/Controllers/Admin/ListaEscolarController.php
+++ app/Http/Controllers/Admin/ListaEscolarController.php
@@ -40,8 +40,8 @@ class ListaEscolarController extends Controller
         //dd($request->get("id"));
         //$colegio=DB::table("colegio")->where('id', $request->get("id"))->first();
 
-        $colegio=DB::select("select colegio.id,colegio.nombre as colegio,comunas.nombre as comuna from colegio
-        inner join comunas on colegio.id_comuna = comunas.id where colegio.id=".$request->get("id"))[0];
+        $colegio=DB::select('select colegio.id,colegio.nombre as colegio,comunas.nombre as comuna from colegio
+        inner join comunas on colegio.id_comuna = comunas.id where colegio.id='.$request->get("id").'')[0];
         //dd($colegio);
 
         $cursos=DB::table('curso')->where('id_colegio', $request->get('id'))->get();
