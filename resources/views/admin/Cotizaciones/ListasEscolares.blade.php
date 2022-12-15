acfb48fb7166e2fb7c1aa9be00b033bbf61614d6
Ignacio <ignaciobarrera4@gmail.com>
2022-12-15 12:45:28 -0300

    Test

--- resources/views/admin/Cotizaciones/ListasEscolares.blade.php
+++ resources/views/admin/Cotizaciones/ListasEscolares.blade.php
@@ -29,7 +29,6 @@ Lista Escolar
                                 </div>
                                 <!-- <button type="button" class="btn btn-success btn-sm float-right" id="add_field_button" >Agregar <i class="fas fa-plus"></i></button> -->
                             </div>
-
                             <div class="card-body collapse hide">
 
                             <div class="callout callout-success row">
@@ -54,16 +53,10 @@ Lista Escolar
                                         <div class="col"><input type="text" class="form-control" placeholder="Codigo" name="codigo" required id="codigo"></div>
                                         <div class="col"><input type="text" class="form-control" placeholder="Cantidad" name="cantidad" required id="cantidad"></div>
                                         <div class="col"><button type="submit" class="btn btn-success" >Agregar Item</button></div>
-
-                                        <!-- <div class="col" style="text-algin:right">
-                                            <a href="" title="Cargar Cotizacion" data-toggle="modal" data-target="#nombremodal"
-                                            class="btn btn-info">Cotización</a>
-                                        </div> -->
-
                                     </div>
                                 </form>
                             </div>
-
+                        <!-- Fin Modal agregar Item -->
                         <br>
             <div class="row">
                     <div class="col-md-12">
@@ -109,36 +102,19 @@ Lista Escolar
                                     <td style="text-align:left">${{ number_format(($item->cantidad*$item->preciou), 0, ',', '.') }}</td>
                                     <div style="display: none">{{ $total += $item->precio_detalle }}</div>
                                     <td>
-                                        <div class="container">
-                                        <div class="row">
+                                    <form action="{{ route('EliminarItem')}}" method="post" enctype="multipart/form-data">
+                                        <input type="text" value="{{$item->cod_articulo}}" name="cod_articulo" hidden>
+                                        <input type="text" value="{{$item->id}}" name="id" hidden>
+                                        <input type="text" value="{{ $curso->id }}" name="idcurso" hidden>
+                                        <input type="text" value="{{ $colegio->id }}" name="id_colegio" hidden>
+                                        <button class="btn btn-danger" style="margin-left: 5%" type="submit">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                            </svg>
+                                        </button>
+                                    </form>
 
-                                    <div class="col-4" style="text-algin:right">
-                                        <form action="{{ route('EliminarItem')}}" method="post" enctype="multipart/form-data">
-                                            <input type="text" value="{{$item->cod_articulo}}" name="cod_articulo" hidden>
-                                            <input type="text" value="{{$item->id}}" name="id" hidden>
-                                            <input type="text" value="{{ $curso->id }}" name="idcurso" hidden>
-                                            <input type="text" value="{{ $colegio->id }}" name="id_colegio" hidden>
-                                            <button class="btn btn-danger" style="margin-left: 5%" type="submit">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                                                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-                                                </svg>
-                                            </button>
-                                        </form>
-                                    </div>
-                                    <!-- boton comentar-->
-                                    <div class="col-1" style="text-algin:right">
-
-                                        <a href="" title="Agregar Comentario" data-toggle="modal" data-target="#mimodalejemplo"
-                                        class="btn btn-info"
-                                        data-id='{{ $item->id }}'
-                                        data-comentario='{{ $item->comentario }}'
-
-                                        data-curso='{{ $curso->id }}'
-                                        data-colegio='{{  $colegio->id }}'
-                                        >comentar</a>
-                                    </div>
-                                    <!-- boton comentar-->
                                     </td>
                                 </tr>
                                 @endforeach
@@ -163,71 +139,43 @@ Lista Escolar
           </div>
         </div>
 </div>
-<!-- Modal -->
-<div class="modal fade" id="mimodalejemplo" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-   <div class="modal-dialog" role="document">
-      <div class="modal-content">
-          <div class="modal-header">
-            <h4 class="modal-title" id="myModalLabel">Agregar Comentario</h4>
-          </div>
-           <div class="modal-body">
-            <div class="card-body">
-            <form method="POST" action="{{ route('AgregarComentario')}}">
-            {{ method_field('put') }}
-            {{ csrf_field() }}
-             @csrf
-                <div class="form-group row">
-                    <label for="comentario"
-                    class="col-md-4 col-form-label text-md-right">{{ __('Comentario') }}</label>
-
-                    <div class="col-md-6">
-                        <textarea id="comentario" maxlength="65" class="form-control form-control-sm" placeholder="Comentario" name="comentario" rows="3">
-
-                        </textarea>
-                        <input type="text" value="" name="id" id="id" hidden>
-                        <input type="text" value="{{ $curso->id }}" name="idcurso" hidden>
-                        <input type="text" value="{{ $colegio->id }}" name="id_colegio" hidden>
-                        @error('comentario')
-                        <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-                </div>
-                <div class="modal-footer">
-                <button type="submit" class="btn btn-primary">Guardar</button>
-                <button type="button" data-dismiss="modal" class="btn btn-secondary">Cerrar</button>
-                </div>
-            </form>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- FIN Modal -->
-
 @endsection
 
 @section('script')
-
 <script>
-    $('#mimodalejemplo').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget)
-        var id = button.data('id')
-        var comentario = button.data('comentario')
-        var curso = button.data('idcurso')
-        //var cod_articulo = button.data('cod_articulo')
-        var colegio = button.data('id_colegio')
-        var modal = $(this)
-        modal.find('.modal-body #id').val(id);
-        modal.find('.modal-body #comentario').val(comentario);
-        //modal.find('.modal-body #cod_articulo').val(cod_articulo);
-        modal.find('.modal-body #idcurso').val(curso);
-        modal.find('.modal-body #id_colegio').val(colegio);
-})
-</script>
+        /*function guardar(){
+        if ( $('#basic-form')[0].checkValidity() ) {
+            $('#basic-form').submit();
+            $.ajax({
+                url: '../admin/AgregarItem/',
+                type: 'POST',
+                data: {'id_curso': $('#id_curso').val(), 'codigo': $('#codigo').val(), 'cantidad': $('#cantidad').val()},
+                success: function(result) {
+                    console.log(result);
+                    location.reload();
+                }
+            });
+        }else{
+            console.log("formulario no es valido");
+        }
+    }*/
 
-<script>
+    /*function eliminar(id){
+
+        var opcion = confirm("Desea eliminar Item?");
+        if (opcion == true) {
+            $.ajax({
+            url: '../admin/EliminarItem/'+id,
+            type: 'DELETE',
+            success: function(result) {
+
+            }
+            });
+            location.reload();
+            } else {
+
+            }
+    }*/
 
   $(document).ready(function() {
     $('#Listas').DataTable( {
@@ -254,10 +202,8 @@ Lista Escolar
     } );
   } );
   </script>
-
   <link rel="stylesheet" href="{{asset("assets/$theme/plugins/datatables-bs4/css/buttons.dataTables.min.css")}}">
   <link rel="stylesheet" href="{{asset("assets/$theme/plugins/datatables-bs4/css/jquery.dataTables.min.css")}}">
-
   <script src="{{asset("js/jquery-3.3.1.js")}}"></script>
   <script src="{{asset("js/jquery.dataTables.min.js")}}"></script>
   <script src="{{asset("js/dataTables.buttons.min.js")}}"></script>
@@ -267,6 +213,9 @@ Lista Escolar
   <script src="{{asset("js/vfs_fonts.js")}}"></script>
   <script src="{{asset("js/buttons.html5.min.js")}}"></script>
   <script src="{{asset("js/buttons.print.min.js")}}"></script>
-  <script src="{{asset("js/ajaxproductospormarca.js")}}"></script>
+
+
+
+<script src="{{asset("js/ajaxproductospormarca.js")}}"></script>
 
 @endsection
