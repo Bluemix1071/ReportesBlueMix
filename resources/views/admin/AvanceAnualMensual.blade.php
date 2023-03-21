57463ed0dcb3eb51ca1fa11d4222df977f85579a
Ignacio <ignaciobarrera4@gmail.com>
2023-03-21 12:13:44 -0300

    Modificacion avance mensual anual

--- resources/views/admin/AvanceAnualMensual.blade.php
+++ resources/views/admin/AvanceAnualMensual.blade.php
@@ -45,7 +45,7 @@
                         @if (empty($ventasala))
                             <h3>Venta Diaria Sala: $ 0</h3>
                         @else
-                            <h3>Venta Diaria Sala: ${{ number_format($ventasala, 0, ',', '.') }}</h3>
+                            <h3>Venta Diaria Sala: ${{ number_format($ventasala, 0, '.', '.') }}</h3>
                         @endif
                     </div>
                 </div>
