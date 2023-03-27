3c8d1222d2c5fb095c11e559f0398c028626f81f
Ignacio <ignaciobarrera4@gmail.com>
2023-03-27 13:57:16 -0300

    Se modifico avance anual mensual calculos v9

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -2724,6 +2724,8 @@ public function stocktiemporeal (Request $request){
 
         $factuasxnc=$factuasxnca[0]->suma+$factuasxncb[0]->suma;
         $facturasmenosnc=$facturasporcobrar[0]->porcobrar-$factuasxnc;
+        dd($facturasmenosnc+$ventasala);
+
 
 
 
