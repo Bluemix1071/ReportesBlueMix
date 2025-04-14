35d4e47be5b45d2d66ba3af840164b7ae1238802
Rokax1 <leandrofernando739@gmail.com>
2025-04-14 15:53:58 -0400

    agregado modulo de codigos segun guia v2

--- routes/web.php
+++ routes/web.php
@@ -102,7 +102,7 @@ Route::prefix('api')->middleware('auth')->group(function(){
     Route::get('/', 'ApiController@LoadReact')->name('apiReact');
 });
 
-Route::prefix('Despacho')->namespace('despacho')->middleware('auth')->group(function(){
+Route::prefix('Despacho')->namespace('Despacho')->middleware('auth')->group(function(){
     Route::get('/ProductosSegunGuia','DespachoController@ProductosSegunGuia')->name('ProductosSegunGuia');
     Route::post('/GuardarProductosSegunGuia','DespachoController@GuardarProductosSegunGuia')->name('GuardarProductosSegunGuia');
 });
